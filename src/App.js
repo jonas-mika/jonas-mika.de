4cca33882e51cd4ec1b736177986e77c2603bd95
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2022-01-12 23:04:21 +0100

    added 3d hero animation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { useState, useEffect } from 'react';
-import AnimatedCursor from "react-animated-cursor" // custom cursor
-import useLocalStorage from 'use-local-storage' // store information about colorscheme
+// import { useState, useEffect } from 'react';
+import AnimatedCursor from "react-animated-cursor"; // custom cursor
+import useLocalStorage from 'use-local-storage'; // store information about colorscheme
 import './styles/index.scss';
 
 import Header from './components/Header';
+import Hero from './components/Hero';
+import Projects from './components/Projects';
 
 function App() {
     const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
     const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
-
-    const [darkmode, setDarkmode] = useState(false);
 
     const toggleTheme = () => {
         setTheme(theme === 'dark' ? 'light' : 'dark');
@@ -20,12 +20,15 @@ function App() {
             <AnimatedCursor 
                 innerSize={10}
                 outerSize={10}
-                color={theme === 'light' ? '135, 0, 0' : '8, 180, 4'}
+                color={'0, 170, 255'}
                 outerAlpha={0.1}
                 innerScale={0.5}
                 outerScale={0}
             />
+            <Hero theme={theme}/>
+            <div className='Dummy'></div>
             <Header theme={theme} toggleTheme={toggleTheme}/>
+            <Projects theme={theme}/>
         </div>
     );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,148 @@
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import { Canvas, extend, useFrame, useLoader, useThree } from 'react-three-fiber';
+import circleImg from '../assets/circle.png';
+import { Suspense, useCallback, useMemo, useRef, useState, useEffect } from 'react';
+import {MdArrowDropDown} from 'react-icons/md';
+extend({OrbitControls})
+
+
+/*
+function CameraControls(){
+  const {
+    camera,
+    gl: {domElement}
+  } = useThree();
+
+  const controlsRef = useRef();
+  useFrame(() => controlsRef.current.update())
+
+  return (
+    <orbitControls
+      args={[camera, domElement]}
+      ref={controlsRef}
+      autoRotate
+      autoRotateSpeed={-0.5}
+    />
+  );
+}
+*/
+
+function Points( { theme } ) {
+  const imgTex = useLoader(THREE.TextureLoader, circleImg);
+  const bufferRef = useRef();
+
+  let t = 0;
+  let f = 0.001;
+  let a = 3;
+  const graph = useCallback((x, y) => {
+    return Math.sin(f * (x ** 2 + y ** 2 + t)) * a;
+  }, [t, f, a, theme])
+
+  const count = 100
+  const sep = 3
+  let positions = useMemo(() => {
+    let positions = []
+
+    for (let xi = 0; xi < count; xi++) {
+      for (let yi = 0; yi < count; yi++) {
+        let x = sep * (xi - count / 2);
+        let y = sep * (yi - count / 2);
+        let z = graph(x, y);
+        positions.push(x, z, y);
+      }
+    }
+
+    return new Float32Array(positions);
+  }, [count, sep, graph, theme])
+
+  useFrame(() => {
+    t += 15
+    
+    const positions = bufferRef.current.array;
+
+    let i = 0;
+    for (let xi = 0; xi < count; xi++) {
+      for (let zi = 0; zi < count; zi++) {
+        let x = sep * (xi - count / 2);
+        let z = sep * (zi - count / 2);
+
+        positions[i + 1] = graph(x, z);
+        i += 3;
+      }
+    }
+
+    bufferRef.current.needsUpdate = true;
+  })
+
+  return (
+    <points>
+      <bufferGeometry attach="geometry">
+        <bufferAttribute
+          ref={bufferRef}
+          attachObject={['attributes', 'position']}
+          array={positions}
+          count={positions.length / 3}
+          itemSize={3}
+        />
+      </bufferGeometry>
+
+      <pointsMaterial
+        attach="material"
+        map={imgTex}
+        color={0x00AAFF}
+        size={0.5}
+        sizeAttenuation
+        transparent={false}
+        alphaTest={0.5}
+        opacity={1.0}
+      />
+    </points>
+  );
+}
+
+function AnimationCanvas({ theme }) {
+  /*
+  const [yPos, setYPos] = useState(0);
+
+  useEffect(() => {
+    const onScroll = () => {
+      setYPos(window.pageYOffset);
+      console.log(yPos);
+    };
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [yPos]);
+  */
+
+  return (
+    <Canvas
+      colorManagement={false}
+      camera={{ position: [100, 10, 0], fov: 75 }}
+    >
+      <Suspense fallback={null}>
+        <Points theme={theme} />
+      </Suspense>
+    </Canvas>
+  );
+}
+
+
+const Hero = ({ theme } ) => {
+  return (
+    <div className="Hero">
+      <div className="main-container">
+        <h2 className="header bold no-select secondary">data is driving the world</h2>
+        <div className="icon">
+            <MdArrowDropDown/>
+        </div>
+      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <AnimationCanvas theme={theme}/>
+      </Suspense>
+    </div>
+  );
+}
+
+export default Hero;
