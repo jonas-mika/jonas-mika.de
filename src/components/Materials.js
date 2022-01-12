0d78252dbe14c1d8fa33f8a015224f1a5daae45a
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2022-01-12 23:29:00 +0100

    skeleton for projects and materials section

---
+++ src/components/Materials.js
@@ -0,0 +1,12 @@
+
+const Materials = ({ theme }) => {
+    return (
+        <div id="Materials" className="Material">
+            <div className="main-container">
+                <p>Projects</p>
+            </div>
+        </div>
+    );
+}
+
+export default Materials;
---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+const Projects = ({ theme }) => {
+    return (
+        <div id="Projects" className="Projects">
+            <div className="main-container">
+                <p>Projects</p>
+            </div>
+        </div>
+    );
+}
+
+export default Projects;
