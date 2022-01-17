ac48ae25277c8fef6a7ded123bdb8a058af6f64c
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2022-01-17 01:12:47 +0100

    connected loading of resource to heroku backend and added formatting of code blocks

--- src/components/Resource.js
+++ src/components/Resource.js
@@ -1,40 +1,81 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+
+import { CopyBlock, CodeBlock, dracula } from "react-code-blocks";
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import 'github-markdown-css';
 
+const FILETYPES = {
+        "md": "markdown",
+        "txt": "txt",
+
+        // coding
+        "py": "python",
+        "c": "c",
+        "cpp": "cpp",
+        "r": "r",
+        "sql": "sql",
+        "rust": "rust", 
+        "js": "javascript",
+        
+    }
+
 const Resource = ({ course, setShowBackground }) => {
     const { resource } = useParams();
-    const [data, setData] = useState(null);
+    const [state, setState] = useState({
+        "fetched": false,
+        "data": null,
+        "filetype": null
+    });
+
+    useEffect(async () => {
+        // fetching resource data
+        const data = fetch(`https://jonas-mika.herokuapp.com/api/assets/courses/${course}/${resource}`)
+                        .then(res => res.text())
+                        //.then(data => data.resolve())
+                        .then(data => {return data});
 
 
-    /*
-    const fetchResource = () => {
-        import(`${process.env.PUBLIC_URL}/public/content/${course}/${resource}`)
-        .then(res => {
-            fetch(res.default)
-                .then(res => res.text())
-                .then(res => setData(res))
-                .catch(err => console.log(err));
-        })
-        .catch(err => console.log(err));
-    }
-    
-    useEffect(() => {
-        fetchResource();
-        setShowBackground(false);
+        // getting filetype
+        const [name, suffix] = resource.split('.');
+        const filetype = FILETYPES[suffix];
+
+        setState({
+            "fetched": true,
+            "data": await data,
+            "filetype": filetype
+        });
     }, []);
-    */
+
+
+    const render = () => {
+        console.log('rendering', state);
+        if (state.fetched) {
+            if (state.filetype === 'markdown' || state.filetype === 'txt') {
+                return <ReactMarkdown 
+                         children={state.data} 
+                         remarkPlugins={[remarkGfm]}
+                         skipHtml={true}
+                        />
+            } else {   
+                return (<CodeBlock 
+                    text={state.data}
+                    language={state.filetype}
+                    showLineNumbers={false}
+                    theme={dracula}
+                    wrapLines
+                />)
+            }
+        } else {
+            return (<div>Loading...</div>)
+        }
+    }
 
     return (
         <div id="Resource" className="Resource">
             <div className="markdown-body" style={{marginTop: '3rem'}}>
-                <ReactMarkdown 
-                    children={data} 
-                    remarkPlugins={[remarkGfm]}
-                    skipHtml={true}
-                />
+                {render()}
             </div>
         </div>)
 }
